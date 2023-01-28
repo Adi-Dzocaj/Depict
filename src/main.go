0df86ebe1a8ddeea7af63c01614f3817c1d8ec16
Adi <adidzocaj@gmail.com>
2023-01-28 21:52:23 +0100

    Initial commit

--- src/main.go
+++ src/main.go
@@ -1,59 +1,42 @@
 package main
 
 import (
-	"fmt"
 	"image"
-	"image/jpeg"
+	"image/color"
+	"image/png"
+	"log"
 	"os"
 )
 
-var exampleImg string = "./images/parisz.jpg"
-
-func init() {
-	// damn important or else At(), Bounds() functions will
-	// caused memory pointer error!!
-	image.RegisterFormat("jpeg", "jpeg", jpeg.Decode, jpeg.DecodeConfig)
-}
-
+var exampleImg string = "./images/yellowCar.png"
 func main() {
-	imgfile, err := os.Open(exampleImg)
+	const width, height = 256, 256
 
-	if err != nil {
-		fmt.Println("exampleImg file not found!")
-		os.Exit(1)
-	}
+	// Create a colored image of the given width and height.
+	img := image.NewNRGBA(image.Rect(0, 0, width, height))
 
-	defer imgfile.Close()
-
-	// get image height and width with image/jpeg
-	// change accordinly if file is png or gif
-
-	imgCfg, _, err := image.DecodeConfig(imgfile)
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	width := imgCfg.Width
-	height := imgCfg.Height
-
-	fmt.Println("Width : ", width)
-	fmt.Println("Height : ", height)
-
-	// we need to reset the io.Reader again for image.Decode() function below to work
-	// otherwise we will  - panic: runtime error: invalid memory address or nil pointer dereference
-	// there is no build in rewind for io.Reader, use Seek(0,0)
-	imgfile.Seek(0, 0)
-
-	// get the image
-	img, _, err := image.Decode(imgfile)
-
-	fmt.Println(img.At(10, 10).RGBA())
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
-			r, g, b, a := img.At(x, y).RGBA()
-			fmt.Printf("[X : %d Y : %v] R : %v, G : %v, B : %v, A : %v  \n", x, y, r, g, b, a)
+			img.Set(x, y, color.NRGBA{
+				R: uint8((x + y) & 255),
+				G: uint8((x + y) << 1 & 255),
+				B: uint8((x + y) << 2 & 255),
+				A: 255,
+			})
 		}
+	}
+
+	f, err := os.Create(exampleImg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err := png.Encode(f, img); err != nil {
+		f.Close()
+		log.Fatal(err)
+	}
+
+	if err := f.Close(); err != nil {
+		log.Fatal(err)
 	}
 }
